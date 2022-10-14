57c7d85544410e9886c141599699a299d941377a
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-15 04:47:17 +0900

    [Bronze IV] Title: 주사위 세개, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,16 +1,8 @@
-dice = [int(i) for i in input().split()]
+dice = sorted(list(map(int, input().split())))
 
-dice_set = list(set(dice))
-
-if len(dice_set) == 1:
-    print(10000+(dice_set[0]*1000))
-    
-elif len(dice_set) == 2:
-    
-    for d in dice_set:
-        dice.remove(d)
-    
-    print(1000 + (dice[0]*100))
-    
+if len(set(dice)) == 1:
+    print(10000 + (dice[0]*1000))
+elif len(set(dice)) == 2:
+    print(1000 + (dice[1]*100))    # 3개 중 2개가 중복이므로 중간을 선택한다.
 else:
-    print(max(dice) * 100)
+    print(dice[2]*100)
