b77c547d4b1a9b38ab0c8e7533fa37f2574e4d87
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-15 04:02:35 +0900

    Update 영수증.py
    - add other code

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -10,3 +10,14 @@ if x == 0:
     print('Yes')
 else:
     print('No')
+    
+
+# 다른 코드
+# total은 x와 같다.
+# 0은 False, 0이 아닌 모든 수는 True이다.
+# if total : total이 참이면 (0이 아닌 숫자라면) => False
+
+if total :
+  print("No")
+else :
+  print("Yes")
