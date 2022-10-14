3ce172da6f067e0858403c927e90f7afb6939b48
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-15 05:13:43 +0900

    Update 주사위 세개.py
    - add other code

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,3 +1,6 @@
+# 가장 빠른 답을 보고 수정한 것
+# 68ms
+
 dice = sorted(list(map(int, input().split())))
 
 if len(set(dice)) == 1:
@@ -6,3 +9,25 @@ elif len(set(dice)) == 2:
     print(1000 + (dice[1]*100))    # 3개 중 2개가 중복이므로 중간을 선택한다.
 else:
     print(dice[2]*100)
+    
+    
+    
+# 원래 제출했던 코드
+# 72ms
+
+dice = [int(i) for i in input().split()]
+
+dice_set = list(set(dice))
+
+if len(dice_set) == 1:
+    print(10000+(dice_set[0]*1000))
+    
+elif len(dice_set) == 2:
+    
+    for d in dice_set:
+        dice.remove(d)
+    
+    print(1000 + (dice[0]*100))
+    
+else:
+    print(max(dice) * 100)
