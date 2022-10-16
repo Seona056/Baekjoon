124612c96665947af01ebf935accd74ef64208da
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-17 01:08:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,17 +1,3 @@
-# 첫 번째 답 : 걸린 시간, 메모리는 동일함
-
 n, x = map(int, input().split())
-A = list(filter(lambda a: x > int(a), input().split()))
-
-for a in A:
-    print(int(a), end=' ')
-    
-    
-    
-# 두 번째 답 : 걸린 시간, 메모리는 동일함
-
-n, x = input().split()
-A = list(filter(lambda a: int(x) > int(a), input().split()))
-
-for a in A:
-    print(int(a), end=' ')
+A = [i for i in input().split() if x > int(i)]
+print(' '.join(A))
