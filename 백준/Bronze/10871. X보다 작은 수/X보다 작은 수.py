da034a0bbbaddad30fa427ca6b573e1446c8a444
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-17 00:05:43 +0900

    Update X보다 작은 수.py

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,3 +1,15 @@
+# 첫 번째 답 : 걸린 시간, 메모리는 동일함
+
+n, x = map(int, input().split())
+A = list(filter(lambda a: x > int(a), input().split()))
+
+for a in A:
+    print(int(a), end=' ')
+    
+    
+    
+# 두 번째 답 : 걸린 시간, 메모리는 동일함
+
 n, x = input().split()
 A = list(filter(lambda a: int(x) > int(a), input().split()))
 
