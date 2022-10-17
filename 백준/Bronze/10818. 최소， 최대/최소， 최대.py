f81f8288774bd4c9d79ac127300bd5c5f4d9f419
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-18 01:33:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 408 ms, Memory: 148408 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,32 +1,5 @@
-# 첫 번째 답
-# 메모리: 149432 KB, 시간: 716 ms
-
-n = input()
-nums = sorted(list(map(int, input().split())))
-
-print(nums[0])
-print(nums[-1])
-
-
-
-# 두 번째 답
-# 메모리: 148408 KB, 시간: 408 ms
-# 가장 빠른 답
-# 리스트 컴프리헨션보다 map이 더 빨랐다.
-
 n = input()
 nums = list(map(int, input().split()))
-
-print(min(nums))
-print(max(nums))
-
-
-
-# 두 번째 답
-# 메모리: 149432 KB, 시간: 472 ms
-
-n = input()
-nums = [int(i) for i in input().split()]
 
 print(min(nums))
 print(max(nums))
