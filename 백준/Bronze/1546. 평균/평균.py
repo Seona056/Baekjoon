0f0e43ed8bc75217ec81efb46d83cbef27f09e8e
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-17 20:01:56 +0900

    Update 평균.py

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,3 +1,14 @@
+# 첫 번째 답
+# 메모리, 시간 동일
+
+n = int(input())
+score = list(map(int, input().split()))
+print(sum([s/max(score) * 100 for s in score])/n)
+
+
+# 두 번째 답
+# 메모리, 시간 동일
+
 n = int(input())
 score = list(map(int, input().split()))
 print(sum([s/max(score) * 100 for s in score])/n)
