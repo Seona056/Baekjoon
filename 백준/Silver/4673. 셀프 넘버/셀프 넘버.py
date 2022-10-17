0a8d90b6f3ca6114563d49507f9aa30919a50126
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-18 03:05:21 +0900

    [Silver V] Title: 셀프 넘버, Time: 560 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -0,0 +1,15 @@
+def generator(num: int):
+    sum_numbers = 0
+    for n in str(num):
+        sum_numbers += int(n)
+    return num + sum_numbers
+
+self_num = []
+
+for i in range(1, 10001):
+    new = generator(i)
+    self_num.append(new)
+    if i in self_num:
+        pass
+    else:
+        print(i)
