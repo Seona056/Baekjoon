9b23a24a9bbb0cd190b0811142421245c6936790
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-21 06:15:16 +0900

    Update 한수.py

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -11,7 +11,7 @@ if n < 100:
     print(n)
 elif 100 <= n < 1000:
     for i in range(100, n+1):
-        [a, b, c] = [int(j) for j in str(i)]      # str로 for문은 돌릴 수 있지만, map은 사용 못 함
+        [a, b, c] = [int(j) for j in str(i)]      
         if b-a == c-b:
             count += 1
     print(count)
