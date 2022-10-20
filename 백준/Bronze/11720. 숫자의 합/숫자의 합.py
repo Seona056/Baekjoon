79464fc30292e36b24924b1e2ef1105428728560
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-21 06:17:10 +0900

    Update 숫자의 합.py

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,2 +1,20 @@
+# 첫 번째 답
+# 메모리: 30840 KB, 시간: 68 ms
+# 일반적인 for문
+
+n = input()
+nums = input() 
+total = 0
+
+for i in nums:
+    total += int(i)
+print(total)
+
+
+# 두 번째 답
+# 메모리: 30840 KB, 시간: 68 ms
+# 시간, 메모리는 똑같음
+# 가장 빠른 답을 참고한 코드
+
 input()
 print(sum(map(int, input())))
