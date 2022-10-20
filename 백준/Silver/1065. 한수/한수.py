8cdab57a9c5cb1e86afda8d2ed33f3c2b2c5f654
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-21 06:10:28 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -8,7 +8,7 @@ if n < 100:
     print(n)
 elif 100 <= n < 1000:
     for i in range(100, n+1):
-        [a, b, c] = [int(j) for j in str(i)]      # str로 for문은 돌릴 수 있지만, map은 사용 못 함
+        [a, b, c] = map(int, str(i))      
         if b-a == c-b:
             count += 1
     print(count)
