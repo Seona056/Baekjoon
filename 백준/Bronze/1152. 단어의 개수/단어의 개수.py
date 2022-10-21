5e7833090e6df31d633b4b7859372fed1bd501df
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-22 04:26:51 +0900

    Update 단어의 개수.py

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,3 +1,17 @@
+# 첫 번째 답
+# 메모리: 32796 KB, 시간: 80 ms
+
+import sys
+
+s = list(sys.stdin.readline().split())
+print(len(s))
+
+
+# 두 번째 답
+# 메모리: 32796 KB, 시간: 72 ms
+# strip()을 사용하여 공백 제거
+# len()보다 count()를 사용하는 것이 더 빨랐다.
+
 import sys
 
 s = sys.stdin.read().strip()   # 양쪽 끝의 공백 제거
