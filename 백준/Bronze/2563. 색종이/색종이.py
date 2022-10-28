9214097b4d8dc74834de54a4ca163feb43b07667
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-10-29 01:35:44 +0900

    [Bronze I] Title: 색종이, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2563. 색종이/색종이.py
+++ 백준/Bronze/2563. 색종이/색종이.py
@@ -3,13 +3,17 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-paper = [tuple(map(int, input().split())) for _ in range(n)]
-max_x = max(paper, key=lambda x: x[0])[0]
-max_y = max(paper, key=lambda y: y[1])[1]
-area = [(max_x+10)*[0] for _ in range(max_y+10)]
+X, Y = [], []
 
-for p in paper:
-    x, y = p[0]-1, p[1]-1
+for _ in range(n):
+    a, b = map(int, input().split())
+    X.append(a)
+    Y.append(b)
+
+area = [(max(X)+10)*[0] for _ in range(max(Y)+10)]
+
+for x, y in zip(X, Y):
+    x, y = x-1, y-1
     for i in range(y, y+10):
         area[i][x:x+10] = [1]*10
 
