dadbf9ba51d109054e7412f17f41ebaad34d1a04
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-01 03:05:59 +0900

    [Bronze I] Title: 소인수분해, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,7 +1,11 @@
 n = int(input())
+i = 2
+r = int(n**0.5)   # root
 
-for i in range(2, n+1):
-    if n % i == 0:
-        while n % i == 0:
-            n /= i
-            print(i)
+while i <= r:
+    while not n % i:   # while not 구문: 조건문이 참이 아닐 동안 반복되는 while문 (n%i==0은 False)
+        n //= i
+        print(i)
+    i += 1
+if n > 1:
+    print(n)
