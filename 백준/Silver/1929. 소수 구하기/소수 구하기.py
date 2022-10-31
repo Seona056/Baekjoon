47953696ddc5761349f6b5e17195a9a4b6689c9e
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-01 04:12:33 +0900

    [Silver III] Title: 소수 구하기, Time: 440 ms, Memory: 46188 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,9 @@
 f, l = map(int, input().split())   # first, last
-root = lambda x: int(x**0.5)+1
+decimal = [False, False]+[True]*(l-1)   # index 0, 1을 Fasle로 설정 (소수가 아님)
 
-for n in range(f, l+1):
-    if n < 2:
-        continue
-        
-    c = 0
-    for i in range(2, root(n)):
-        if n % i == 0:
-            c = 1
-            break
-    if c == 0:
-        print(n)
+for i in range(2, l+1):
+    if decimal[i]:
+        if f <= i:
+            print(i)
+        for j in range(i*2, l+1, i):  # 소수 i의 배수를 모두 Fasle로 바꾼다.
+            decimal[j] = False
