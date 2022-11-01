92aad4e2f22d690ec5fbfe9bc3e3b341517be37b
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-01 21:25:31 +0900

    Update 소수 구하기.py

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -26,11 +26,11 @@ for n in range(f, l+1):
 # 첫 번째 답보다 시간은 1/5가량 줄었지만, 메모리는 조금 더 사용되었다.
 
 f, l = map(int, input().split())   # first, last
-decimal = [False, False]+[True]*(l-1)   # index 0, 1을 Fasle로 설정 (소수가 아님)
+prime = [False, False]+[True]*(l-1)   # index 0, 1을 Fasle로 설정 (소수가 아님)
 
 for i in range(2, l+1):
-    if decimal[i]:
+    if prime[i]:
         if f <= i:
             print(i)
         for j in range(i*2, l+1, i):  # 소수 i의 배수를 모두 Fasle로 바꾼다.
-            decimal[j] = False
+            prime[j] = False
