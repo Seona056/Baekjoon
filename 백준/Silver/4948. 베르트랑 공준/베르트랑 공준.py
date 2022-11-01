03ca95202288d88c8092ea206c7ff32d0c5ee9ab
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-01 21:22:27 +0900

    [Silver III] Title: 베르트랑 공준, Time: 804 ms, Memory: 34700 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,19 +1,18 @@
 import sys
-input = sys.stdin.readline
 
-while True:
-    try:
-        n = int(input())
-        if n == 0:
-            break
-        decimal = [False, False]+[True]*(2*n-1)
-        c = 0   # count
-        for i in range(2, 2*n+1):
-            if decimal[i]:
-                if i > n:
-                    c += 1
-                for j in range(2*i, 2*n+1, i):
-                    decimal[j] = False
-        print(c)
-    except:
+numbers = list(map(int, sys.stdin.readlines()))
+m = max(numbers)
+decimal = []
+
+l = [False, False]+[True]*(2*m-1)  # list
+for i in range(2, 2*m+1):
+    if l[i]:
+        decimal.append(i)
+        for j in range(2*i, 2*m+1, i):
+            l[j] = False
+            
+for n in numbers:
+    if n == 0:
         break
+    answer = list(filter(lambda x: n < x <= 2*n, decimal))
+    print(len(answer))
