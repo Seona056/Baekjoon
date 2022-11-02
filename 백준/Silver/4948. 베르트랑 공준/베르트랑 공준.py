2b8d3454dac5bdbb8780e3f9e09bfbea8f65ca91
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-02 17:08:46 +0900

    Update 베르트랑 공준.py

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -35,14 +35,14 @@ while True:
     n = int(input())
     if n == 0:
         break
-    decimal = [False, False]+[True]*(2*n-1)
+    prime = [False, False]+[True]*(2*n-1)
     c = 0   # count
     for i in range(2, 2*n+1):
-        if decimal[i]:
+        if prime[i]:
             if i > n:
                 c += 1
             for j in range(2*i, 2*n+1, i):
-                decimal[j] = False
+                prime[j] = False
     print(c)
 
     
@@ -102,19 +102,19 @@ import sys
 
 numbers = list(map(int, sys.stdin.readlines()))
 m = max(numbers)
-decimal = [2]
+prime = [2]
 
 l = [False, False]+[True]*(2*m-1)  # list
 for i in range(3, 2*m+1, 2):   # 짝수는 모두 소수가 아니므로, 홀수만 검증
     if l[i]:
-        decimal.append(i)
+        prime.append(i)
         for j in range(2*i, 2*m+1, i):
             l[j] = False
             
 for n in numbers:
     if n == 0:
         break
-    answer = list(filter(lambda x: n < x <= 2*n, decimal))
+    answer = list(filter(lambda x: n < x <= 2*n, prime))
     print(len(answer))
 
 
