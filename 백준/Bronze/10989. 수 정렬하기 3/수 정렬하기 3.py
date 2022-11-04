7d08a5354a41d70765b260af7547b6c7a67cad63
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-05 03:18:26 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8756 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,13 +1,11 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.buffer.readline
 
-t = int(input())
-check = [0]*10001   # index 0은 사용 x
+check = [0]*10001   # index 0은 사용x (1~10000)
 
-for i in range(t):
-    n = int(input())
-    check[n] += 1   # n-1의 카운트를 올리면 index값이 n과 달라짐
-    
+for i in range(int(input())):
+    check[int(input())] += 1
+
 for x in range(10001):
     if check[x] != 0:
         for y in range(check[x]):
