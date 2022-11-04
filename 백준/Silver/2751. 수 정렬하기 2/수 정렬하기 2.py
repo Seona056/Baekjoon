3edbb5ea0fb79619bc3ec83fe84bc9b5074bfa37
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-05 01:50:58 +0900

    Update 수 정렬하기 2.py

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -4,4 +4,4 @@ input = sys.stdin.readlines
 num = list(map(int, input()))
 num = sorted(num[1:])
 
-print('\n'.join(map(str,num)))
+print('\n'.join(map(str,num)))   # '구분자'.join(리스트) : 리스트의 요소는 문자열이어야 한다. 👉 하나의 문자열로 반환
