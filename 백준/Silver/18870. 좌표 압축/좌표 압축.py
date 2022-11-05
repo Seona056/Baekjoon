e633adfdff1fb6f696855d7c638fdb39870ba016
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-06 04:11:09 +0900

    [Silver II] Title: 좌표 압축, Time: 1736 ms, Memory: 199496 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -3,8 +3,6 @@ input = sys.stdin.readline
 
 input()
 nums = list(map(int, input().split()))
-nums_idx = sorted(list(set(nums)))
-nums_dict = {v:i for i, v in enumerate(nums_idx)}
+nums_dict = {v:i for i, v in enumerate(sorted(list(set(nums))))}
 
-for n in nums:
-    print(nums_dict[n], end=' ')    
+print(' '.join(map(str, [nums_dict[n] for n in nums])))   
