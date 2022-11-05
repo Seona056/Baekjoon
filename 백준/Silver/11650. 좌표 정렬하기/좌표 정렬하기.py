e9e882a230b2438e4abebaa861495c80d9409bba
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-06 00:22:43 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 184 ms, Memory: 43332 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,9 +1,9 @@
-coordinates = []
+import sys
 
-for i in range(int(input())):
-    x, y = map(int, input().split())
-    coordinates.append((x, y))
+def sort_num(n):
+    x, y = n.split()
+    return int(x) + int(y)/1000000   # 100,000 최고 입력값이므로, 나눴을 때 1 이하가 되도록 0을 하나 더 붙여준다.
 
-coordinates.sort()
-for c in coordinates:
-    print(*c)
+coordinates = sys.stdin.readlines()[1:]
+coordinates.sort(key=lambda n: sort_num(n))
+print(''.join(coordinates))
