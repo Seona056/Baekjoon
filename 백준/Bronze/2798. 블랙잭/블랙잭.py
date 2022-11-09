b53836f524ee6ab83b2a92765aef0326ed26b015
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-10 01:07:01 +0900

    Update 블랙잭.py

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,3 +1,8 @@
+# 프로그래머스 [12977. 소수 만들기]와 같은 방법으로 풀었음
+# ✔ itertools.combinations(리스트, 숫자) ✔
+# 더 빠른 답을 찾아보니 3중 for문으로 해결... 너무 원시적인 방법 아닌가 해서 itertools.combinations를 씀
+# 내 코드가 마음에 든다👍 깰끔✨
+
 import sys
 import itertools
 
