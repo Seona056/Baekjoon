9acf1c5433d6fee89a1ab583ebd96cca58bb4c6f
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-09 14:49:31 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,13 +1,12 @@
 import sys
 n = int(sys.stdin.readline())
+pre = 0
+f = 1
 if n == 0:
     print(0)
 elif n == 1:
     print(1)
 else:
-    pre = 0
-    f = 1
     for i in range(n-1):
-        f += pre
-        pre = f-pre
+        pre, f = f, pre+f
     print(f)
