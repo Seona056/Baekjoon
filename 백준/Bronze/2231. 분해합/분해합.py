6e94ae3153f3276e743fa829d2aa4401a84c8670
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-13 03:29:10 +0900

    [Bronze II] Title: 분해합, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,11 +1,2 @@
-n = int(input())
-m = n//2
-
-while m < n:
-    decomposition = sum(map(int ,str(m))) + m
-    if decomposition == n:
-        print(m)
-        break
-    m += 1
-else:
-    print(0)
+num = int(input())
+print(min([n for n in range(max(num-54, num//2), num) if sum(map(int, str(n)))+n == num], default=0))
