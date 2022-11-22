10e3161093df0416f7dfbd953401a0e23011091b
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-23 02:50:26 +0900

    Update 체스판 다시 칠하기.py

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,28 +1,33 @@
+# 블로그 검색을 한 답
+# 내가 짠 코드와 거의 비슷했지만, 크로스로 검증하는 코드에서 제대로 되지 않았다...
+# if (a+b) % 2 != 0 이라는 마법의 코드를 배웠다고 생각하자...
+
 import sys
 
 board = sys.stdin.readlines()
 n, m = map(int, board[0].split())
-board = list(map(lambda x: x.rstrip(),board[1:]))
+board = list(map(lambda x: x.rstrip(), board[1:]))
 color_count = []
 
+color_count = []
 
-
-for a in range(n-7):
-    for b in range(m-7):
-        index1 = 0
-        index2 = 0
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if board[i][j] != 'W':
-                        index1 += 1
-                    if board[i][j] != 'B':
-                        index2 += 1
-                else:
-                    if board[i][j] != 'B':
-                        index1 += 1
-                    if board[i][j] != 'W':
-                        index2 += 1
-        color_count.append(min(index1, index2))
+for i in range(n-7):
+    
+	for j in range(m-7):
+		c_white, c_black = 0, 0
+        
+		for a in range(i, i+8):
+			for b in range(j, j+8):
+				if (a+b) % 2 == 0:                  # 이 조건문으로 각 배열이 두 칸씩, 다음 줄은 크로스로 두 칸씩 검사
+					if board[a][b] != 'W':
+						c_white += 1
+					elif board[a][b] != 'B':
+						c_black += 1
+				else:
+					if board[a][b] != 'B':
+						c_white += 1
+					elif board[a][b] != 'W':
+						c_black += 1
+		color_count.append(min(c_white, c_black))
 
 print(min(color_count))
