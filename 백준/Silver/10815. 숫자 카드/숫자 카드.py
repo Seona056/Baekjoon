4af4411400783531f4cfbb87429791ad5a1ea720
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-24 04:19:49 +0900

    [Silver V] Title: 숫자 카드, Time: 388 ms, Memory: 128012 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,10 +1,7 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readlines
 
-s = int(input())
-sangun = set(map(int, input().split()))
-input()
-card = list(map(int, input().split()))
-
-for c in card:
-	print(1, end=' ') if c in sangun else print(0, end=' ')
+sangun, card = sys.stdin.read().split('\n')[1::2]	# idx 1, 3 요소를 각각의 변수에 담는다.
+sangun = set(sangun.split())
+card = card.split()
+print(' '.join('1' if c in sangun else '0' for c in card))    # join은 str만 가능하므로 1, 0 str로 설정
