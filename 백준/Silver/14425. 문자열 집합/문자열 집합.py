9b0e34d35543fb9b33fa9123775fc4ec46502d15
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-24 18:15:38 +0900

    [Silver III] Title: 문자열 집합, Time: 104 ms, Memory: 50580 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -5,4 +5,8 @@ S = sys.stdin.read().split()
 check = S[int(S[0])+2:]
 S = set(S[2:int(S[0])+2])
 
-print(len([c for c in check if c in S]))
+c = 0    # count
+for ch in check:
+    if ch in S:
+        c += 1
+print(c)
