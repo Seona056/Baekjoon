8d8b9fe8148f7b82ac70f37ad9ee8ab6a576225c
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-24 18:18:01 +0900

    Update 문자열 집합.py

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,3 +1,23 @@
+# 첫 번째 답
+# 메모리: 50712 KB, 시간: 104 ms
+# 리스트의 len을 구하는 방법으로 작성
+# 두 번째 답과 시간은 같지만, 메모리가 조금 더 소요됨
+
+import sys
+
+S = sys.stdin.read().split()
+
+check = S[int(S[0])+2:]
+S = set(S[2:int(S[0])+2])
+
+print(len([c for c in check if c in S]))
+
+
+# 두 번째 답
+# 메모리: 50580 KB, 시간: 104 ms
+# 새로운 리스트를 만들지 않아서 메모리가 조금 적게 소요됨
+# 소요시간은 위와 동일
+
 import sys
 
 S = sys.stdin.read().split()
