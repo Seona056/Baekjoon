8f82fdd03dbac4ac615ebd203f89d9f86f80a0b0
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-28 21:53:44 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 228 ms, Memory: 68556 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,8 @@
+m, a, *pokemon = open(0).read().split()
+
+dogam_idx = {i+1:p for i, p in enumerate(pokemon[:int(m)])}
+dogam_pokemon = {p:i+1 for i, p in enumerate(pokemon[:int(m)])}
+question = pokemon[int(m):]
+
+for q in question:
+	print(dogam_pokemon[q]) if q.isalpha() == True else print(dogam_idx[int(q)])
