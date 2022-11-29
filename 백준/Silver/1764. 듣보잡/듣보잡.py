6af6b5367bc61b5fda76fa03ec17a98c38b15953
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-11-29 18:33:32 +0900

    [Silver IV] Title: 듣보잡, Time: 100 ms, Memory: 43588 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,10 +1,8 @@
-import sys
-
 n, m, *people = open(0).read().split()
-
-print(int(n)+int(m) - len(set(people)))
 
 no_hear = set(people[:int(n)])
 no_see = set(people[int(n):])
+intersection = sorted(no_hear & no_see)
 
-print('\n'.join(sorted(set.intersection(no_hear, no_see))))
+print(len(intersection))
+print('\n'.join(intersection))
