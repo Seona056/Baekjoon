56f5c126b7082904cbec6818040f0f317a692f34
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-12-20 05:34:19 +0900

    Update 검문.py

--- 백준/Gold/2981. 검문/검문.py
+++ 백준/Gold/2981. 검문/검문.py
@@ -1,3 +1,7 @@
+# 나머지가 같은 수를 구하기 위해서는, 오름차순으로 정렬한 뒤 두 수의 차를 구한다.
+# a-b, b-c, c-d
+# 이 차들의 최대 공약수를 구한 뒤, 최대 공약수의 약수들을 출력 (1 제외)
+
 import sys
 import math
 
@@ -12,7 +16,7 @@ for i in range(n-1):
 gcd = math.gcd(*sub)    
 f = set()
 
-for j in range(2, int(gcd**0.5)+1):
+for j in range(2, int(gcd**0.5)+1):	# 자연수 n의 최대 약수 : sqrt(n)
 	if gcd % j == 0:
 		f.add(j)
 		f.add(gcd//j)
