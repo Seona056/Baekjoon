6632d0685ae1a5c9abc0f4e41d2a177017d5715f
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-12-20 05:27:05 +0900

    [Gold IV] Title: 검문, Time: 40 ms, Memory: 32540 KB -BaekjoonHub

---
+++ 백준/Gold/2981. 검문/검문.py
@@ -0,0 +1,20 @@
+import sys
+import math
+
+nums = sys.stdin.readlines()
+n = int(nums[0])
+nums = sorted(map(int, nums[1:]))[::-1]
+sub = []
+
+for i in range(n-1):
+    sub.append(nums[i] - nums[i+1])
+
+gcd = math.gcd(*sub)    
+f = set()
+
+for j in range(2, int(gcd**0.5)+1):
+	if gcd % j == 0:
+		f.add(j)
+		f.add(gcd//j)
+
+print(*sorted(f), gcd)
