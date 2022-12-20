a53fad89adae7168d4534b08587b9ea986b00982
Kim Seona <86583906+Seona056@users.noreply.github.com>
2022-12-21 04:41:53 +0900

    Update 팩토리얼 0의 개수.py

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,3 +1,9 @@
+# from itertools import groupby를 사용하면 연속된 문자열 그룹을 구할 수 있다.
+# for a, b in groupby('012300000')
+# 👉 a는 해당 문자 : 0
+# 👉 b는 : <itertools._grouper object at 0x14b4081201f0>
+# 👉 b를 리스트로 변환 : list(b) = ['0', '0', '0', '0', '0']
+
 from math import factorial as f
 from itertools import groupby
 
