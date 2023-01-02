53c196e40f824c19e5dc1eb4f743e912d637d011
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-01-03 03:57:12 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -3,23 +3,10 @@ vps = sys.stdin.readlines()[1:]
 
 for v in vps:
 	v = v.rstrip()
-	l, r = 0, 0
+	while '()' in v:
+		v = v.replace('()', '')
 	
-	for i in v:
-		if i == '(':
-			if l >= r:
-				l += 1
-			else:
-				l += 1
-				break
-		else:
-			if l > r:
-				r += 1
-			else:
-				r += 1
-				break
-	
-	if l == r:
-		print('YES')
-	else:
+	if v:
 		print('NO')
+	else:
+		print('YES')
