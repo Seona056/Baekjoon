b1f6a82682872625aa6d89a25e65cb41c4462e0f
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-01-05 05:04:33 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 144 ms, Memory: 34828 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,31 @@
+import sys
+import re
+txt = sys.stdin.readlines()[:-1]
+
+for t in txt:
+	if t[0] == '.':
+		print('no')
+		continue
+    
+	t = re.sub('[a-zA-Z0-9]', '', t)
+	t = ''.join(t.split())
+	
+	a = ['[]', '()']
+	while any(i in t for i in a):
+		if '()' in t:
+			t = t.replace('()', '')
+		if '[]' in t:
+			t = t.replace('[]', '')
+	
+	if t != '.':
+		print('no')
+	else:
+		t = list(t)
+		c = 0
+		for i in range(int(len(t))//2):
+			if t[i] != t.pop():
+					c += 1
+		if c == 0:
+			print('yes')
+		else:
+			print('no')
