d3a7c4ec1a8c7c08cbb0b87eb93198b871e5eafc
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-02-03 01:34:58 +0900

    [Silver IV] Title: 카드2, Time: 180 ms, Memory: 40736 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -4,15 +4,14 @@ n = int(input())
 cards = collections.deque(range(2, n+1, 2))
 
 if n == 1:
-	cards = [1]
+	print(1)
 else:
 	if n % 2 != 0:
 		cards.append(cards[0])
 		cards.popleft()
 
-while len(cards) > 2:
-	cards.append(cards[1])
-	for i in range(2):
-		cards.popleft()
-
-print(cards[-1])
+	while len(cards) > 2:
+		cards.append(cards[1])
+		for i in range(2):
+			cards.popleft()
+	print(cards[-1])
