9ca80ccbba2efcdbfa6077c1833d8e3763943ec4
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-02-08 04:30:14 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,27 @@
+prime = [False, False]+[True]*(10000-1)
+for i in range(2,10000+1):
+    if prime[i]:
+        for j in range(i*2, 10000+1, i):
+            prime[j] = False
+
+            
+import sys
+test = map(int, sys.stdin.readlines()[1:])
+
+for t in test:
+
+    if t == 4:
+        print(2, 2)
+        continue
+
+    half = t//2
+    if prime[half]:
+        print(half, half)
+        continue
+
+    while True:
+        half -= 1
+        if prime[half]:
+            if prime[t-half]:
+                print(half, t-half)
+                break
