d49ee03b1f0683a41cc6a30a798d3ffddb865764
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-03-08 21:53:58 +0900

    [Bronze I] Title: 약수들의 합, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -5,15 +5,12 @@ for n in nums:
 	n = int(n)
 	factors = [1]
 	i = 2
-	while i < n:
+	while i < (n**0.5):
 		if n % i == 0:
-			if i in factors:
-				break
 			factors.append(i)
 			factors.append(n//i)
 		i += 1
 	if n == sum(factors):
-		# factors = sorted(factors)
 		print(f'{n} = ' + ' + '.join(map(str, sorted(factors))))
 	else:
 		print(f'{n} is NOT perfect.')
