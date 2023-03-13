4a17cbbf5482ea47821d31b2ab601723690e63e1
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-03-14 02:43:16 +0900

    [Silver IV] Title: 가로수, Time: 84 ms, Memory: 44160 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,19 +1,5 @@
-import sys
 import math
-
-n = int(input())
-trees = list(map(int, sys.stdin.readlines()))
-gap = []
-
-for i in range(n-1):
-	gap.append(trees[i+1] - trees[i])
-	
-m, c = math.gcd(*gap), 0
-
-for g in gap:
-	if g == m:
-		continue
-	elif g > m:
-		c += g//m - 1
-		
-print(c)
+n, *trees = map(int, open(0).readlines())
+gap = math.gcd(*[x-y for x,y in zip(trees[1:], trees)])
+all = (trees[-1]-trees[0]) // gap + 1
+print(all-n)
