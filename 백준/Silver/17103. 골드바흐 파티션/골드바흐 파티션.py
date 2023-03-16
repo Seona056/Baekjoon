b4056f42a7bed443579d654ec46256c14eeb197c
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-03-16 23:08:03 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 196 ms, Memory: 138568 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,21 +1,21 @@
 def get_prime(n):
 	prime = [1]*(n+1)
+	prime[4::2] = [0]*(n//2-1)
 	
-	for i in range(2, int(n**0.5)+1):
-		if prime[i]:
-			j = 2
-			while i*j <= n:
-				prime[i*j] = 0
-				j += 1
-	return prime
+	for i in range(3, int(n**0.5)+1, 2):
+		prime[2*i::i] = [0]*(n//i-1)
+	
+	prime_2 = [2]+[i for i in range(3, n+1, 2) if prime[i]]
+	return prime, prime_2
 	
 nums = list(map(int, open(0).readlines()[1:]))
-prime = get_prime(max(nums))
+prime, prime_2 = get_prime(max(nums))
 
 for n in nums:
-	i, c = 2, 0
-	while i <= n//2:
-		if prime[i] and prime[n-i]:
+	c = 0
+	for p in prime_2:
+		if p > (n//2):
+			print(c)
+			break
+		if prime[n-p]:
 			c += 1
-		i += 1
-	print(c)
