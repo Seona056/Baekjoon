9350a65479f8035b8ac94f076dfbc29d0f86021d
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-03-23 20:09:14 +0900

    [Silver IV] Title: 수 찾기, Time: 144 ms, Memory: 48724 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,30 +1,3 @@
-def binary(a:list, t:int, s:int, e:int):
-	'''
-	a : list
-	t : target
-	s : start
-	e : end
-	m : mid
-	'''
-	if s > e:
-		return 0
-	
-	m = (s+e)//2
-	
-	if a[m] == t:
-		return 1
-	elif a[m] > t:
-		return binary(a, t, s, m-1)
-	else:
-		return binary(a, t, m+1, e)
-		
-import sys
-input = sys.stdin.readline
-
-n = int(input())
-a = sorted(map(int, input().split()))
-input()
-
-if __name__ == '__main__':	
-	for i in input().split():
-		print(binary(a, int(i), 0, n-1))
+a, b = open(0).readlines()[1::2]
+a = set(map(int, a.split()))
+print('\n'.join(map(str, [1 if int(x) in a else 0 for x in b.split()])))
