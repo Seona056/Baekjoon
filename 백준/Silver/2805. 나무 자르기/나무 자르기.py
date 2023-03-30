314d46f907b625905771050882be6323a7347397
Kim Seona <86583906+Seona056@users.noreply.github.com>
2023-03-31 05:21:39 +0900

    [Silver II] Title: 나무 자르기, Time: 2060 ms, Memory: 143400 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,18 @@
+def get_m(m, first, end):
+	if first > end:
+		return end
+	
+	mid = (first+end)//2
+	left = sum([t-mid for t in trees if t>mid])
+	
+	if left >= m:
+		return get_m(m, mid+1, end)
+	else:
+		return get_m(m, first, mid-1)
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
+print(get_m(m, 0, max(trees)))
