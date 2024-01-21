05e4a8552d4c28fe5fb2f927a728f3a460e42b08
Kim Seona <86583906+Seona056@users.noreply.github.com>
2024-01-21 16:35:05 +0900

    [Silver IV] Title: 덱 2, Time: 668 ms, Memory: 245312 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -4,38 +4,20 @@ command = open(0).readlines()[1:]
 d = deque([])
 
 for c in command:
-	c = c.rstrip()
-	
+	c = c.split()
 	if c[0] == '1':
-		c, x = map(int, c.split())
-		d.appendleft(x)
+		d.appendleft(int(c[-1]))
 	elif c[0] == '2':
-		c, x = map(int, c.split())
-		d.append(x)
+		d.append(int(c[-1]))
 	elif c[0] == '3':
-		if d:
-			print(d.popleft())
-		else:
-			print(-1)
+		print(d.popleft() if d else -1)
 	elif c[0] == '4':
-		if d:
-			print(d.pop())
-		else:
-			print(-1)
-	elif c[0] == '5':
+		print(d.pop() if d else -1)
+	elif c[0]== '5':
 		print(len(d))
 	elif c[0] == '6':
-		if d:
-			print(0)
-		else:
-			print(1)
+		print(0 if d else 1)
 	elif c[0] == '7':
-		if d:
-			print(d[0])
-		else:
-			print(-1)
+		print(d[0] if d else -1)
 	elif c[0] == '8':
-		if d:
-			print(d[-1])
-		else:
-			print(-1)
+		print(d[-1] if d else -1)
