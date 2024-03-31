8ba6ffcfd749c6675ac11b2ddf1bed2e948b75e5
Kim Seona <86583906+Seona056@users.noreply.github.com>
2024-03-31 21:44:21 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -2,16 +2,13 @@ def solution(dots):
     answer = 0
     line1 = [(0,1), (0,2), (0,3)]
     line2 = [(2,3), (1,3), (1,2)]
-    
+
     for l1, l2 in zip(line1,line2):
-        d1 = dots[l1[0]]
-        d2 = dots[l1[1]]
-        d3 = dots[l2[0]]
-        d4 = dots[l2[1]]
-        
-        a = list(map(lambda x, y: y-x, d1, d2))
-        b = list(map(lambda x, y: y-x, d3, d4))
-        
-        if a[1]/a[0] == b[1]/b[0]:
+        [x1, y1] = dots[l1[0]]
+        [x2, y2] = dots[l1[1]]
+        [x3, y3] = dots[l2[0]]
+        [x4, y4] = dots[l2[1]]
+
+        if (x2-x1)/(y2-y1) == (x4-x3)/(y4-y3):
             return 1
     return answer
