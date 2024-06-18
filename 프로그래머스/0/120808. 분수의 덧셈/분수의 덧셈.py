4ed49983e9a7ad63dd45f2b3b8e353062717454d
Kim Seona <86583906+Seona056@users.noreply.github.com>
2024-06-18 22:41:25 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,5 +1,5 @@
 from fractions import Fraction
 
 def solution(num1, den1, num2, den2):   
-    frac = Fraction(num1, den1) + Fraction(num2, den2)
-    return [frac.numerator, frac.denominator]
+    answer = Fraction(num1, den1) + Fraction(num2, den2)
+    return [answer.numerator, answer.denominator]
